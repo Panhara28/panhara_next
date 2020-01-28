5478d9aecce650ca0a7e475086d56739f3dd2be0
titpanhara <titpanhara@gmail.com>
2020-01-28 20:38:45 +0700

    fix

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "55c992412c5a1942f444";
+/******/ 	var hotCurrentHash = "602fd38f4761b9628259";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
